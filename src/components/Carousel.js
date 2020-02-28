2fad81f716e64dc327f01d81fdbff43d6a83c1bd
Kim Cyrus Esquibel <kimcyruse@Kims-MacBook-Air.local>
2020-02-27 19:10:36 -0800

    Update Carousel Component
    Imported images to use for Card images. Edited onClick to click on line 83

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,7 +4,11 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 
 // importing images from image folder
-import imageNotAvailable from "../assets/images/imageNotAvailable.png";
+// import imageNotAvailable from "../assets/images/imageNotAvailable.jpg";
+import blackjack from "../assets/images/blackjack.png";
+import colorImage from "../assets/images/colorImage.png";
+import patatapClone from "../assets/images/patatapClone.jpeg";
+
 import Card from "./Card";
 
 // Carousel component for the Projects
@@ -12,7 +16,7 @@ class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // items array with objects
+      // items array with 3 objects
       items: [
         {
           // id of the object
@@ -22,7 +26,7 @@ class Carousel extends React.Component {
           //   information about the project
           subTitle: "Cloning the Patatap Animation",
           //   image of the project
-          imgSrc: imageNotAvailable,
+          imgSrc: patatapClone,
           //   link to the project
           link: "https://github.com/kimcyruse",
           //   boolean if project is selected
@@ -32,7 +36,7 @@ class Carousel extends React.Component {
           id: 1,
           title: "Color Game",
           subTitle: "RGB Color Game",
-          imgSrc: imageNotAvailable,
+          imgSrc: colorImage,
           link: "https://github.com/kimcyruse",
           selected: false
         },
@@ -40,7 +44,7 @@ class Carousel extends React.Component {
           id: 2,
           title: "Black Jack",
           subTitle: "A Black Jack Game",
-          imgSrc: imageNotAvailable,
+          imgSrc: blackjack,
           link: "https://github.com/kimcyruse",
           selected: false
         }
@@ -48,13 +52,15 @@ class Carousel extends React.Component {
     };
   }
 
+  // arrow function expression
   // a method that handles if the Card(Project) is selected
-  // that take in an id
+  // that take in an id and a card
   handleCardClick = (id, card) => {
-    // get us all the items in the state.items and store it to items
+    // get us all the items in the state.items array and store it to items
     let items = [...this.state.items];
 
-    // if selected set it to false, if not set it to true
+    // ternary expression
+    // if items[id].selected set it to false, if not set it to true
     // to toggle Card on and off
     items[id].selected = items[id].selected ? false : true;
 
@@ -72,7 +78,7 @@ class Carousel extends React.Component {
     });
   };
 
-  //  a method that creates each items
+  //  a method that creates Card component
   //   which take in items
   makeItems = items => {
     return items.map(item => {
@@ -80,7 +86,7 @@ class Carousel extends React.Component {
         // Card component and passing item, onClick method and key
         <Card
           item={item}
-          onClick={e => this.handleCardClick(item.id, e)}
+          click={e => this.handleCardClick(item.id, e)}
           key={item.id}
         />
       );
