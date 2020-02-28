7c7231c55aacc9cd419ea610f4fac4ab8105727c
Kim Cyrus Esquibel <kimcyruse@Kims-MacBook-Air.local>
2020-02-27 19:12:35 -0800

    Add comments

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,9 @@ import Carousel from "../components/Carousel";
 function HomePage(props) {
   return (
     <div>
+      {/* Hero component passing title, subTitle, text */}
       <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
+      {/* Carousel component */}
       <Carousel />
     </div>
   );
