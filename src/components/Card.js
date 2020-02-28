6a11d7d55d1d381036a6701f1f00a37dc79da26d
Kim Cyrus Esquibel <kimcyruse@Kims-MacBook-Air.local>
2020-02-27 18:29:45 -0800

    Add elements to the Card components
    We added images and Card information to the Card component. With onClick listener, the Card information is shown when the Card is selected

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,28 @@
 import React from "react";
 
+import CardInfo from "./CardInfo";
+
 function Card(props) {
   return (
-    <div>
-      <img className="" src={props.item.imgSrc} alt={props.item.imgSrc} />
+    // d-inline-block - so cards are all next to each other
+    // onClick listener - calls props.click that takes in an item which is (props.item)
+    <div
+      className="d-inline-block k-card"
+      onClick={e => props.click(props.item)}
+    >
+      <img
+        className="k-card-image"
+        src={props.item.imgSrc}
+        alt={props.item.imgSrc}
+      />
+      {/* conditionally rendered - means don't show it unless selected */}
+      {props.item.selected && (
+        <CardInfo
+          title={props.item.title}
+          subTitle={props.item.subTitle}
+          link={props.item.link}
+        />
+      )}
     </div>
   );
 }
