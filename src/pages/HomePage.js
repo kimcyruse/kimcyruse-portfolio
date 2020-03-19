6b1b5aa1fecd7069e4b2cf1c92038d76493fbb1d
Kim Cyrus Esquibel <kimcyruse@Kims-Air.hitronhub.home>
2020-03-18 23:51:09 -0700

    Delete Carousel component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,12 @@
 import React from "react";
 
 import Hero from "../components/Hero";
-import Carousel from "../components/Carousel";
 
 function HomePage(props) {
   return (
     <div>
       {/* Hero component passing title, subTitle, text */}
       <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
-      {/* Carousel component */}
-      <Carousel />
     </div>
   );
 }
