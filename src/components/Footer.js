1c372d7e13bb552c92300b17400308d70caa30e5
Kim Cyrus Esquibel <kimcyruse@Kims-Air.hitronhub.home>
2020-04-09 22:16:40 -0700

    Add Social Media Icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import Col from "react-bootstrap/Col";
 function Footer() {
   return (
     // mt-5 - margin top 5
-    <footer className="mt-5">
+    <footer className="mt-5 footer fixed-bottom">
       {/* fluid={true} - to use full screen */}
       <Container fluid={true}>
         {/* justify-content-between - creates space between content item */}
@@ -15,14 +15,16 @@ function Footer() {
           {/* p-0 - padding 0 */}
           {/* sm = {3} - use 3 grid in md */}
           <Col className="p-0" sm={3}>
-            Kim Cyrus Esquibel
+            <i class="fab fa-github"></i>
+            <i class="fab fa-linkedin-in"></i>
+            <i class="fas fa-envelope"></i>
           </Col>
           {/* p-0 - padding 0 */}
           {/* d-flex - create simple flexbox container*/}
           {/* justify-content-end - put the content at end */}
           {/* md = {8} - use 8 grid in md */}
           <Col className="p-0 d-flex justify-content-end" sm={8}>
-            © 2019 designed and developed by Kim Cyrus Esquibel
+            ©Kim Cyrus Esquibel
           </Col>
         </Row>
       </Container>
